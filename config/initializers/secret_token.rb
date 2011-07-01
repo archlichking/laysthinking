aeb7922ba78e43bf8e5ce421549a0113bde16276
Lei Zhu <thunderzhulei@gmail.com>
2011-07-01 22:07:11 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laysthinking::Application.config.secret_token = '3e82e55adcf638901e374e8caface717c51dcaa583e4f4f5073fff470d3946b00070095c6379377cf0bf5544dee66d234ddb65f5ca8ee5f0111b0151c3311485'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Laysthinking::Application.config.session_store :cookie_store, :key => '_laysthinking_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Laysthinking::Application.config.session_store :active_record_store
