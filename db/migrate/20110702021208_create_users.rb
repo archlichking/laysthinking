bd4c7f43a1d8f8f64e40fbadd793d6df401bd8c5
Lei Zhu <thunderzhulei@gmail.com>
2011-07-02 12:19:44 +0800

    first sample

---
+++ db/migrate/20110702021208_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+      t.string :nick
+      t.datetime :created_at
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
